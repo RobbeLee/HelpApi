f1fbd0a841726728a226e47af1b9b165c0743ba2
Robbe <26785@ma-web.nl>
2020-09-08 13:47:07 +0200

    Updated gitignore and added Controller
    Added the commands controller and added .DS_store to the gitignore because useless mac files should be deleted.

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using HelpApi.Data;
+using HelpApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpApi.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockHelpApiRepo _repository = new MockHelpApiRepo();
+        //GET api/commands
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+
+            return Ok(commandItems);
+        }
+
+        //GET api/commands/5
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(CommandItem);
+        }
+    }
+}
